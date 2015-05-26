1f7e547f471599be20c0bbec96cf21f9a6477e90
Juntao Liu <jinuljt@gmail.com>
2015-05-26 11:14:00 +0800

    add emacs go eldoc

--- .emacs.d/init/init_go.el
+++ .emacs.d/init/init_go.el
@@ -7,11 +7,18 @@
 
 (require 'go-mode-load)
 (add-hook 'before-save-hook 'gofmt-before-save)
-;(add-hook 'go-mode-hook (lambda ()
-;                          (local-set-key (kbd \"M-.\") 'godef-jump)))
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") 'godef-jump)))
 (add-to-list 'load-path "~/.emacs.d/plugins/goflymake")
 (require 'go-flymake)
 
 (add-to-list 'load-path "~/.emacs.d/plugins/gocode/emacs")
+(require 'auto-complete-config)
 (require 'go-autocomplete)
+(ac-config-default)
+
+(add-to-list 'load-path "~/.emacs.d/plugins/emacs-go-eldoc")
+(require 'go-eldoc)
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+
 ;;;;go-lang end
